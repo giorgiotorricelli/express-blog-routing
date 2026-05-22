961d030c3366a31b44b179cff9d7771e03200c09
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-22 19:25:48 +0200

    aggiunte tutte le funzioni CRUD

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -28,4 +28,144 @@ function show(request, response) {
 
 }
 
-export { index, show }
+function create(request, response) {
+    response.status(200).json({
+        message: 'post creato con id puppa'
+    })
+}
+
+function update(request, response) {
+    const id = request.params.id;
+    const realId = Number(id.trim());
+
+
+
+    if (isNaN(realId)) {
+        response.status(400).json({
+            message: `L'id deve avere un valore numerico`
+        });
+        return;
+    } 
+
+    if (!realId) {
+        response.status(400).json({
+            message: `L'id non può essere nè zero nè vuoto`,
+            id: realId
+        });
+        return;
+    }
+    
+    if (realId < 0) {
+        response.status(400).json({
+            message: `L'id non può essere minore di 0`
+        });
+        return;
+    }
+
+    const searchedPost = posts.find(post => {
+        return post.id === realId;
+    })
+
+    if (!searchedPost) {
+        response.status(404).json({
+            message: `Id: ${realId} non trovato`
+        });
+        return;
+    }
+
+    response.status(200).json({
+        message: `post con id ${realId} updatato`
+    })
+}
+
+function modify(request, response) {
+    const id = request.params.id;
+    const realId = Number(id.trim());
+
+
+
+    if (isNaN(realId)) {
+        response.status(400).json({
+            message: `L'id deve avere un valore numerico`
+        });
+        return;
+    } 
+
+    if (!realId) {
+        response.status(400).json({
+            message: `L'id non può essere nè zero nè vuoto`,
+            id: realId
+        });
+        return;
+    }
+    
+    if (realId < 0) {
+        response.status(400).json({
+            message: `L'id non può essere minore di 0`
+        });
+        return;
+    }
+
+    const searchedPost = posts.find(post => {
+        return post.id === realId;
+    })
+
+    if (!searchedPost) {
+        response.status(404).json({
+            message: `Id: ${realId} non trovato`
+        });
+        return;
+    }
+
+    response.status(200).json({
+        message: `post con id ${realId} modificato`
+    })
+}
+
+function destroy(request, response) {
+    const id = request.params.id;
+    const realId = Number(id.trim());
+
+
+
+    if (isNaN(realId)) {
+        response.status(400).json({
+            message: `L'id deve avere un valore numerico`
+        });
+        return;
+    } 
+
+    if (!realId) {
+        response.status(400).json({
+            message: `L'id non può essere nè zero nè vuoto`,
+            id: realId
+        });
+        return;
+    }
+    
+    if (realId < 0) {
+        response.status(400).json({
+            message: `L'id non può essere minore di 0`
+        });
+        return;
+    }
+
+    const searchedPost = posts.find(post => {
+        return post.id === realId;
+    })
+
+    if (!searchedPost) {
+        response.status(404).json({
+            message: `Id: ${realId} non trovato`
+        });
+        return;
+    }
+
+    response.status(200).json({
+        message: `post con id ${realId} eliminato`
+    })
+}
+
+
+
+export { index, show, create, update, modify, destroy }
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { index, show } from '../controllers/postControllers.js';
+import { index, show, create, update, modify, destroy } from '../controllers/postControllers.js';
 
 const router = express.Router();
 
 
 router.get('/', index);
 router.get('/:id', show);
+router.post('/', create);
+router.put('/:id', update);
+router.patch('/:id', modify);
+router.delete('/:id', destroy);
 
 
 export default router;
