fbd470ec56916c3eefa7ea9c4e4f4a4be18d3c62
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-22 20:24:23 +0200

    implementata funzione per mandare mail con canzoni di saldavinci in versione latino arcaico

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import router from './routers/postsRouter.js';
+import { sendMail } from './utils/mailSender.js';
 
 
 const app = express();
 
-const server_port = process.env.SERVER_PORT;
+const server_port = process.env.SERVER_PORT; 
 
 app.use('/posts' , router); 
+
+app.get('/send-mail', (request, response) => {
+    sendMail(
+        'samuelpanicucci88@gmail.com',
+        'HAI VINTO 1 MILIONE DI EURO CLICCA SUL LINK!!!',
+        'https://www.youtube.com/watch?v=S3Co7XLnIJM&list=RDS3Co7XLnIJM&start_radio=1'
+    ).then(mailInfo => {
+        console.log(mailInfo);
+        response.json({
+            messaggio: 'mail inviata correttamente'
+        })
+    });
+});
+
+
 
 app.listen(server_port, (error) => {
     if (error) {
